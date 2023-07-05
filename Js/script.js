f9d22841bba7283beca6875f56e4efb6aa9bbe68
abdulmex <steveabdulmex@gmail.com>
2023-07-05 15:06:44 +0100

    Modal-Window

--- Js/script.js
+++ Js/script.js
@@ -5,7 +5,7 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal; i++)
+for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener("click", function () {
     console.log("btton clicked");
   });
